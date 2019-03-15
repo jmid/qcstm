b1ac7e7aa5d10229853bcc8f5d1075bfecfaf6bc
Jan Midtgaard <mail@janmidtgaard.dk>
2019-03-15 17:55:25 +0100

    clean up hashtable example

--- examples/hashtable.ml
+++ examples/hashtable.ml
@@ -15,17 +15,27 @@ struct
     | Mem of string
     | Length [@@deriving show { with_path = false }]
 
-  (*  gen_cmd : string Gen.t -> int Gen.t -> command Gen.t  *)
-  let gen_cmd strgen intgen =
+  (*  gen_cmd : state -> command Gen.t  *)
+  let gen_cmd s =
+    let int_gen = Gen.small_int in
+    let str_gen =
+      if s=[]
+      then Gen.oneof [Gen.small_string;
+                      Gen.string]
+      else
+        let keys = List.map fst s in
+        Gen.oneof [Gen.oneofl keys;
+                   Gen.small_string;
+                   Gen.string] in
     Gen.oneof
       [ Gen.return Clear;
-        Gen.map2 (fun k v -> Add (k,v)) strgen intgen;
-        Gen.map  (fun k   -> Remove k) strgen;
-        Gen.map  (fun k   -> Find k) strgen;
-        Gen.map  (fun k   -> Find_opt k) strgen;
-        Gen.map  (fun k   -> Find_all k) strgen;
-        Gen.map2 (fun k v -> Replace (k,v)) strgen intgen;
-        Gen.map  (fun k   -> Mem k) strgen;
+        Gen.map2 (fun k v -> Add (k,v)) str_gen int_gen;
+        Gen.map  (fun k   -> Remove k) str_gen;
+        Gen.map  (fun k   -> Find k) str_gen;
+        Gen.map  (fun k   -> Find_opt k) str_gen;
+        Gen.map  (fun k   -> Find_all k) str_gen;
+        Gen.map2 (fun k v -> Replace (k,v)) str_gen int_gen;
+        Gen.map  (fun k   -> Mem k) str_gen;
         Gen.return Length; ]
 
   let shrink c = let open Iter in match c with
@@ -43,54 +53,40 @@ struct
     | Mem k      -> Iter.map (fun k' -> Mem k') (Shrink.string k)
     | Length     -> Iter.empty
 
-  let arb_cmd s =
-    let str_gen =
-      if s=[]
-      then (Gen.small_string : string Gen.t)
-      else Gen.oneof [Gen.oneofl (List.map fst s);
-                      Gen.small_string] in
-    QCheck.make ~print:show_cmd ~shrink:shrink (gen_cmd str_gen Gen.small_int)
+  let arb_cmd s = QCheck.make ~print:show_cmd ~shrink:shrink (gen_cmd s)
 
   let init_state  = []
-  let init_sut () = Hashtbl.create ~random:false 42
-  let cleanup _   = ()
-
   let next_state c s = match c with
     | Clear         -> []
     | Add (k,v)     -> (k,v)::s
-    | Remove k  -> List.remove_assoc k s
-    | Replace (k,v) ->
-      let rec replace h = match h with
-        | [] -> [(k,v)]
-        | (k',v')::h' ->
-          if k = k' (*&& k<>68*) (* an arbitrary, injected bug *)
-          then (k,v)::h'
-          else (k',v')::replace h' in
-      replace s
+    | Remove k      -> List.remove_assoc k s
+    | Replace (k,v) -> (k,v)::(List.remove_assoc k s)
     | Find _
     | Find_opt _
     | Find_all _
     | Mem _
     | Length        -> s
 
+  let init_sut () = Hashtbl.create ~random:false 42
+  let cleanup _   = ()
   let run_cmd c s h = match c with
     | Clear         -> Hashtbl.clear h; true
     | Add (k,v)     -> Hashtbl.add h k v; true
     | Remove k      -> Hashtbl.remove h k; true
-    | Find k        ->
-      List.assoc_opt k s
-        = (try Some (Hashtbl.find h k)
-           with Not_found -> None)
-    | Find_opt k    ->
-      List.assoc_opt k s = Hashtbl.find_opt h k
+    | Find k        -> List.assoc_opt k s
+                       = (try Some (Hashtbl.find h k)
+                          with Not_found -> None)
+    | Find_opt k    -> List.assoc_opt k s = Hashtbl.find_opt h k
     | Find_all k    ->
+(*    List.map snd (List.find_all (fun p -> fst p = k) s)
+        = Hashtbl.find_all h k *)
       let rec find_all h = match h with
         | [] -> []
         | (k',v')::h' ->
-          if k = k' (*&& k<>68*) (* an arbitrary, injected bug *)
+          if k = k' (*&& k<>"a"*) (* an arbitrary, injected bug *)
           then v'::find_all h'
           else find_all h' in
-      find_all s = Hashtbl.find_all h k
+        find_all s = Hashtbl.find_all h k
     | Replace (k,v) -> Hashtbl.replace h k v; true
     | Mem k         -> List.mem_assoc k s = Hashtbl.mem h k
     | Length        -> List.length s = Hashtbl.length h
@@ -99,4 +95,5 @@ struct
 end
 module HT = QCSTM.Make(HConf)
 ;;
-QCheck_runner.run_tests ~verbose:true [HT.agree_test ~count:10_000 ~name:"Hashtbl-model agreement"]
+QCheck_runner.run_tests ~verbose:true
+  [HT.agree_test ~count:10_000 ~name:"Hashtbl-model agreement"]
