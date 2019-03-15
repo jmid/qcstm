971e32744becb60ec47cb3837c550ce2ea30f26f
Jan Midtgaard <mail@janmidtgaard.dk>
2019-03-15 18:15:04 +0100

    reorder

--- examples/putget.ml
+++ examples/putget.ml
@@ -7,6 +7,7 @@ struct
   type cmd = Put of int | Get [@@deriving show { with_path = false }]
   type state = int
   type sut = Dl.library * (int -> unit) * (unit -> int)
+
   let arb_cmd s =
     let int_gen = Gen.oneof [Gen.map Int32.to_int int32.gen; Gen.small_int] in
     let shrink c = match c with
@@ -16,6 +17,10 @@ struct
       (Gen.oneof [Gen.map (fun i -> Put i) int_gen; Gen.return Get])
       
   let init_state  = 0
+  let next_state c s = match c with
+    | Put i -> i
+    | Get   -> s
+
   let init_sut () =
     let stub =
       Dl.dlopen ~filename:"_build/examples/putgetstub.so" ~flags:[Dl.RTLD_LOCAL;Dl.RTLD_NOW;] (*[Dl.RTLD_LAZY; Dl.RTLD_GLOBAL]*) in
@@ -25,12 +30,10 @@ struct
     
   let cleanup (stub,_,_) = Dl.dlclose stub
       
-  let next_state c s = match c with
-    | Put i -> i
-    | Get   -> s
   let run_cmd c s (_,put,get) = match c with
     | Put i -> (put i; true)
     | Get   -> (get () = s)
+
   let precond _ _ = true
 end
 module PGtest = QCSTM.Make(PGConf)
